c8f61c11e43915b18f9d8181a860919d88215167
SrikanthSaravanan27 <137416006+SrikanthSaravanan27@users.noreply.github.com>
2024-03-03 22:01:00 +0530

    Add files via upload
    insert using recursion

--- LL.java
+++ LL.java
@@ -1,6 +1,4 @@
 public class LL {
-
-
     private Node head;
     private Node tail;
     private int size;
@@ -117,7 +115,18 @@ public class LL {
         }
         return temp;
     }
-
+    public void insertRec(int val,int index){
+        head = insertRec(val,index,head);
+    }
+    private Node insertRec(int val,int index,Node node){
+        if(index == 0){
+            Node temp = new Node(val,node);
+            size++;
+            return temp;
+        }
+        node.next = insertRec(val,--index,node.next);
+        return node;
+    }
     public void display(){
         Node temp = head;
         while(temp != null){
